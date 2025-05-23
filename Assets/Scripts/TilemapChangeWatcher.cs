561ea87ce11e619c07f1140d0469454f0edc4db5
wolfheat <99273780+wolfheat@users.noreply.github.com>
2025-05-23 12:15:21 +0200

    tidied
    * Tidied up changeWatcher code

--- Assets/Scripts/TilemapChangeWatcher.cs
+++ Assets/Scripts/TilemapChangeWatcher.cs
@@ -1,15 +1,9 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using NUnit.Framework;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-
-
-
 
 #if UNITY_EDITOR
 using UnityEditor;
-#endif
 
 [ExecuteAlways]
 public class TilemapChangeWatcher : MonoBehaviour
@@ -17,8 +11,10 @@ public class TilemapChangeWatcher : MonoBehaviour
     public Tilemap tilemap;
     public Tilemap3D tilemap3d;
 
-    private BoundsInt previousBounds;
     private TileBase[] previousTiles;
+    private int[] previousRotations;
+
+    Dictionary<Vector3Int, Sprite> previousSprites = new();
 
     private void OnEnable()
     {
@@ -30,7 +26,7 @@ public class TilemapChangeWatcher : MonoBehaviour
 
 
         var bounds = tilemap.cellBounds;
-        CacheCurrentTiles(bounds);
+        CacheCurrentTiles(bounds, GetTilemapRotations(tilemap,bounds));
         EditorApplication.update += OnEditorUpdate;
     }
 
@@ -45,51 +41,88 @@ public class TilemapChangeWatcher : MonoBehaviour
             return;
 
         var tileMapMode = tilemap3d.UpdateMode;
-        
-        if (tileMapMode == AutoUpdateMode.OFF)
-            return;
+        var bounds = tilemap.cellBounds;
+        var currentTiles = tilemap.GetTilesBlock(bounds);
+        var currentRotations = GetTilemapRotations(tilemap,bounds);
 
-        if(tileMapMode == AutoUpdateMode.INSTANT) {
-            // Check tiles...
-            var bounds = tilemap.cellBounds;
-            var currentTiles = tilemap.GetTilesBlock(bounds);
+        switch (tileMapMode) {
+            case AutoUpdateMode.OFF:
+                return;
+            case AutoUpdateMode.ON:
+                break;
+            case AutoUpdateMode.INSTANT:
+                // Check tiles...
+                if (!AreTilesEqual(currentTiles, previousTiles, currentRotations)) {
+                    //Debug.Log($"Tilemap '{tilemap.name}' changed!");
+                    CacheCurrentTiles(bounds, currentRotations);
+                    //OnTilemapChangedUpdateAll();
+                    OnTilemapChangedUpdateAll();
+                }
+                break;
+            case AutoUpdateMode.INSTANTFAST:
+                // Check tiles...
+                if (!AreTilesEqualGetChanges(currentTiles, previousTiles, bounds, currentRotations, out List <Vector2Int> changedPositions)) {
+                    //Debug.Log($"Tilemap '{tilemap.name}' changed!");
+                    CacheCurrentTiles(bounds, currentRotations);
+                    //OnTilemapChangedUpdateAll();
+                    OnTilemapChangedUpdateOnlyAffectedTiles(changedPositions);
 
-            if (!AreTilesEqual(currentTiles, previousTiles, bounds, out List <Vector2Int> changedPositions)) {
-                //Debug.Log($"Tilemap '{tilemap.name}' changed!");
-                CacheCurrentTiles(bounds);
-                //OnTilemapChangedUpdateAll();
-                OnTilemapChangedUpdateAll();
-            }
-        }
-        if(tileMapMode == AutoUpdateMode.INSTANTFAST) {
-            // Check tiles...
-            var bounds = tilemap.cellBounds;
-            var currentTiles = tilemap.GetTilesBlock(bounds);
-
-            if (!AreTilesEqual(currentTiles, previousTiles, bounds, out List <Vector2Int> changedPositions)) {
-                //Debug.Log($"Tilemap '{tilemap.name}' changed!");
-                CacheCurrentTiles(bounds);
-                //OnTilemapChangedUpdateAll();
-                OnTilemapChangedUpdateOnlyAffectedTiles(changedPositions);
-
-            }
+                }
+                break;
         }
     }
 
-    Dictionary<Vector3Int, Sprite> previousSprites = new();
-    private void CacheCurrentTiles(BoundsInt bounds)
+    private void CacheCurrentTiles(BoundsInt bounds, int[] currentRotations)
     {
         previousTiles = tilemap.GetTilesBlock(bounds);
+        previousRotations = currentRotations;
+
         previousSprites.Clear();
         foreach (var pos in bounds.allPositionsWithin) {
             previousSprites[pos] = tilemap.GetSprite(pos);
         }
     }
-    bool AreTilesEqual(TileBase[] currentTiles, TileBase[] previousTiles, BoundsInt bounds, out List<Vector2Int> changedPositions)
+
+    private int[] GetTilemapRotations(Tilemap tilemap, BoundsInt bounds)
+    {
+        int[] rotations = new int[bounds.size.x * bounds.size.y];
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                float currentRotation = tilemap.GetTransformMatrix(new Vector3Int(x, y, 0)).rotation.eulerAngles.z;
+                int rotationInt = Mathf.RoundToInt(currentRotation + 360) % 360;
+                rotations[bounds.size.x * y + x] = rotationInt;
+            }
+        }
+        return rotations;
+    }
+
+    bool AreTilesEqual(TileBase[] currentTiles, TileBase[] previousTiles, int[] currentRotations)
+    {        
+        if (currentTiles == null || previousTiles == null || currentTiles.Length != previousTiles.Length || previousRotations.Length != currentRotations.Length) {
+            return true;
+        }
+
+        // The amount of tiles are the same
+        for (int i = 0; i < currentTiles.Length; i++) {
+            TileBase current = currentTiles[i];
+            TileBase previous = previousTiles[i];
+
+            int currentRotation = currentRotations[i];
+            int previousRotation = previousRotations[i];
+            
+            if (current != previous || currentRotation != previousRotation) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool AreTilesEqualGetChanges(TileBase[] currentTiles, TileBase[] previousTiles, BoundsInt bounds, int[] currentRotations, out List<Vector2Int> changedPositions)
     {
         changedPositions = new List<Vector2Int>();
 
-        if (currentTiles == null || previousTiles == null || currentTiles.Length != previousTiles.Length) {
+        // Update ALL
+        if (currentTiles == null || previousTiles == null || currentTiles.Length != previousTiles.Length || previousRotations.Length != currentRotations.Length) {
             // All tiles are considered changed
             for (int x = 0; x < bounds.size.x; x++) {
                 for (int y = 0; y < bounds.size.y; y++) {
@@ -99,12 +132,16 @@ public class TilemapChangeWatcher : MonoBehaviour
             return false;
         }
 
+        // Get the positions to update
         for (int x = 0; x < bounds.size.x; x++) {
             for (int y = 0; y < bounds.size.y; y++) {
                 int index = x + y * bounds.size.x;
                 TileBase current = currentTiles[index];
                 TileBase previous = previousTiles[index];
-                if (current != previous) {
+                int currentRotation = currentRotations[index];
+                int previousRotation = previousRotations[index];
+
+                if (current != previous || currentRotation != previousRotation) {
                     changedPositions.Add(new Vector2Int(bounds.xMin + x, bounds.yMin + y));
                     continue;
                 }
@@ -122,19 +159,17 @@ public class TilemapChangeWatcher : MonoBehaviour
         }
 
         return changedPositions.Count == 0;
-
-
     }
-    Sprite GetCachedSprite(TileBase tile, Vector3Int pos, Tilemap tilemap)
-    {
-        return previousSprites.TryGetValue(pos, out var sprite) ? sprite : null;
-    }
+
+    Sprite GetCachedSprite(TileBase tile, Vector3Int pos, Tilemap tilemap) => previousSprites.TryGetValue(pos, out var sprite) ? sprite : null;
+
     private void OnTilemapChangedUpdateOnlyAffectedTiles(List<Vector2Int> changedPositions)
     {
         if (tilemap3d == null)
             tilemap3d = GetComponent<Tilemap3D>();
         tilemap3d.Generate3DTilesForcedSpecific(changedPositions);
     }
+
     private void OnTilemapChangedUpdateAll()
     {
         Debug.Log($"Detected tile change on: {name}");
@@ -145,3 +180,4 @@ public class TilemapChangeWatcher : MonoBehaviour
         tilemap3d.Generate3DTilesForced();
     }
 }
+#endif
